37549df75eb33b1ada23886763fef0ed7db3849d
lukidp12 <lukidwiprasanti98@gmial.com>
2021-12-07 10:16:27 +0700

    Update datatutor.php

--- ngilmu-web/datatutor.php
+++ ngilmu-web/datatutor.php
@@ -21,7 +21,7 @@ $sesName = $_SESSION['email'];
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Data Guru</title>
+    <title>Data Tutor</title>
 </head>
 <body>
     <div class="container-dash">
@@ -93,7 +93,7 @@ $sesName = $_SESSION['email'];
 
         <div class="col-md-12 p-5 pt-2">
             <h2><i class="bi bi-people"></i></i> DATA TUTOR </h2><hr>
-            <a href="" class="btn btn-primary mb-3"><i class="fas fa-plus-square mr-2"></i>TAMBAH DATA TUTOR</a>
+            <a href="tutor/tambahdatatutor.php" class="btn btn-primary mb-3"><i class="fas fa-plus-square mr-2"></i>TAMBAH DATA TUTOR</a>
             <table class="table table-striped table-bordered">
               <thead>
                 <tr>
