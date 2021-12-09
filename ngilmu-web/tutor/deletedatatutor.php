76422b56c30b1246f2c1bab644b3f63c716d82dc
lukidp12 <lukidwiprasanti98@gmial.com>
2021-12-09 22:47:53 +0700

    Create deletedatatutor.php

---
+++ ngilmu-web/tutor/deletedatatutor.php
@@ -0,0 +1,15 @@
+<?php 
+
+require ('../koneksi.php');
+// 	$id = $_GET['id'];
+// 	$email = $row['email'];
+//     $password = $row['password'];
+//     $fullname = $row['fullname'];
+//     $grade = $row['grade'];
+//     $gender = $row['gender'];
+//     $alamat = $row['alamat'];
+// mysqli_query($koneksi, "DELETE FROM user_tutor WHERE '$id' = id_tutor") or die (mysql_error());
+mysqli_query($koneksi, "DELETE FROM user_tutor WHERE id_tutor = '$_GET[id_user]'") or die (mysql_error());
+header('Location: ../datatutor.php ');
+
+?>
