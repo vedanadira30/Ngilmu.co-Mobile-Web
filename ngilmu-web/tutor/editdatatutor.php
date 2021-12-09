2478b5f4e9234cf6c258f8f1e3a6616de9226d18
lukidp12 <lukidwiprasanti98@gmial.com>
2021-12-10 00:05:39 +0700

    Update editdatatutor.php

--- ngilmu-web/tutor/editdatatutor.php
+++ ngilmu-web/tutor/editdatatutor.php
@@ -52,7 +52,7 @@ if (isset($_POST['update'])) {
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Data Siswa</title>
+    <title>Data Tutor</title>
 </head>
 <body>
     <div class="container-dash">
