da313fb696da1bbbfb9cb72bd91f0cb51cd89d4d
NaufalHisyam18 <80368878+NaufalHisyam18@users.noreply.github.com>
2021-12-20 13:54:31 +0700

    Update dashboard.php

--- ngilmu-web/dashboard.php
+++ ngilmu-web/dashboard.php
@@ -143,12 +143,40 @@ $sesName = $_SESSION['email'];
                         <div class="cardName">Mata Pelajaran</div>
                 </div>
             </div>
-
         </div>
 
+        <!-- slider dahsboard -->
+        <div class="box-dash">
+            <div id="carouselExampleIndicators" class="carousel2 slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="images/dashboard1.jpg" class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                    <img src="images/dashboard2.jpg" class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                    <img src="images/dashboard4.jpg" class="d-block w-100">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+            </div>
+        </div>
     </div>
 
-    </div>
+</div>
 
 <script>
     //menuToogle
