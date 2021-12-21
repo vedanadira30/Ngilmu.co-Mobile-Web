92ac2abe196c7550bdd60e1261a4afd10fdaf2f2
lukidp12 <lukidwiprasanti98@gmial.com>
2021-12-21 20:33:05 +0700

    web

--- ngilmu-web/editprofile.php
+++ ngilmu-web/editprofile.php
@@ -1,5 +1,5 @@
 <?php
-require('../koneksi.php');
+require('koneksi.php');
 
 session_start();
 
@@ -11,24 +11,25 @@ $sesID = $_SESSION['id_admin'];
 $sesName = $_SESSION['email'];  
 
 if (isset($_POST['update'])) {
-//    $id = $_POST['id_tutor'];
-    $idtutor = $_POST['id_tutor'];
-    $mapel = $_POST['mata_pelajaran'];
-    $jenjang = $_POST['jenjang'];
-   $query = "UPDATE tabel_mapel SET id_tutor='$idtutor', jenjang='$jenjang' WHERE mata_pelajaran='$mapel'";
+//    $idadmin = $_POST['id_admin'];
+   $email = $_POST['email'];
+   $password = $_POST['password'];
+   $fullname = $_POST['nama_lengkap'];
+     $query = "UPDATE user_detail SET password='$password', nama_lengkap='$fullname',
+            where email='$email'";
    $result = mysqli_query($koneksi, $query);
-   header('Location: ../datamapel.php');
+   header('Location: dashboard.php');
 }
 
-    $id_mapel = $_GET['id_mapel'];
-    $query = "SELECT * FROM tabel_mapel WHERE id_mapel='$id_mapel'";
+    $id = $_GET['id_admin'];
+    $query = "SELECT * FROM user_admin WHERE id_admin='$id'";
     $result = mysqli_query($koneksi, $query) or die (mysql_error());
     $no = 1;
     while ($row = mysqli_fetch_array($result)){
-        $idtutor = $row['id_tutor'];
-        $mapel = $row['mata_pelajaran'];
-        $jenjang = $row['jenjang'];
-
+        $idadmin = $row['id_admin'];
+        $email = $row['email'];
+        $password = $row['password'];
+        $fullname = $row['nama_lengkap'];
     }
 ?>
 <!DOCTYPE html>
@@ -37,11 +38,11 @@ if (isset($_POST['update'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Data Mapel</title>
+    <title>Data Admin</title>
 </head>
 <body>
     <div class="container-dash">
@@ -54,41 +55,47 @@ if (isset($_POST['update'])) {
                     </a>
                 </li>
                 <li>
-                    <a href="../dashboard.php">
+                    <a href="dashboard.php">
                         <span class="icon"><i class="bi bi-house-door"></i></span>
                         <span class="title">Dashboard</span>
                     </a>
                 </li>
                 <li>
-                    <a href="../datasiswa.php">
+                    <a href="datasiswa.php">
                         <span class="icon"><i class="bi bi-person"></i></span>
                         <span class="title">Data Siswa</span>
                     </a>
                 </li>
                 <li>
-                    <a href="../datatutor.php">
+                    <a href="datatutor.php">
                         <span class="icon"><i class="bi bi-people"></i></span>
                         <span class="title">Data Tutor</span>
                     </a>
                 </li>
                 <li>
-                    <a href="../datamapel.php">
+                    <a href="datamapel.php">
                         <span class="icon"><i class="bi bi-book"></i></span>
                         <span class="title">Mata Pelajaran</span>
                     </a>
                 </li>
                 <li>
-                    <a href="../riwayatpemesanan.php">
+                    <a href="riwayatpemesanan.php">
                         <span class="icon"><i class="bi bi-clock-history"></i></span>
                         <span class="title">Riwayat Pemesanan</span>
                     </a>
                 </li>
                 <li>
-                    <a href="../pendapatantutor.php">
+                    <a href="pendapatantutor.php">
                         <span class="icon"><i class="bi bi-wallet2"></i></span>
                         <span class="title">Pendapatan Tutor</span>
                     </a>
                 </li>
+                <!-- <li>
+                    <a href="../index.php">
+                        <span class="icon"><i class="bi bi-box-arrow-left"></i></span>
+                        <span class="title">Log Out</span>
+                    </a>
+                </li> -->
             </ul>
         </div>
 
@@ -105,18 +112,6 @@ if (isset($_POST['update'])) {
                     <i class="bi bi-search"></i>
                 </label>
             </div>
-            <!-- dropdown -->
-            <div class="dropdown">
-            <button class="dropdown-toggle btn btn-sm " type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <?php echo $_SESSION['email'] ?>   
-            <!-- Profile -->
-            </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                    <li><a class="dropdown-item" href="editprofile.php">Edit Profile</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="logout.php" onclick="return confirm('Anda yakin mau logout ?')">Logout</a></li>
-                </ul>
-            </div>
             <!-- userImg -->
             <div class="user">
                 <img src="../images/img1.jpg">
@@ -124,29 +119,30 @@ if (isset($_POST['update'])) {
         </div>
 
         <div class="col-md-12 p-5 pt-2">
-            <h2><i class="bi bi-person"></i></i> EDIT DATA MAPEL </h2><hr>
-            <!-- <a href="siswa/editdatamapel.php" class="btn btn-primary mb-3"><i class="fas fa-plus-square mr-2"></i>TAMBAH DATA SISWA</a> -->
+            <h2><i class="bi bi-person"></i></i> EDIT DATA ADMIN </h2><hr>
+            <!-- <a href="siswa/tambahdatasiswa.php" class="btn btn-primary mb-3"><i class="fas fa-plus-square mr-2"></i>TAMBAH DATA SISWA</a> -->
             <div class="col-12">
                 <form id="form_validation" method="POST">
                             <div class="form-group form-float">
                                 <div class="form-line">
-                                    <label class="form-label">ID Tutor</label>
-                                    <input type="text" class="form-control" name="id_tutor" value="<?php echo $idtutor;?>" required>
+                                    <label class="form-label">Email</label>
+                                    <input type="email" class="form-control" name="email" value="<?php echo $email;?>" required>
                                 </div>
                             </div>
                             <div class="form-group form-float">
                                 <div class="form-line">
-                                    <label class="form-label">Mapel</label>
-                                    <input type="text" class="form-control" name="mata_pelajaran" value="<?php echo $mapel;?>" required>
+                                    <label class="form-label">Password</label>
+                                    <input type="text" class="form-control" name="password" value="<?php echo $password;?>" required>
                                 </div>
                             </div>
                             <div class="form-group form-float">
                                 <div class="form-line">
-                                    <label class="form-label">Jenjang</label>
-                                    <input type="text" class="form-control" name="jenjang" value="<?php echo $jenjang;?>" required>
+                                    <label class="form-label">Fullname</label>
+                                    <input type="text" class="form-control" name="nama_lengkap" value="<?php echo $fullname;?>" required>
                                 </div>
+                            </div>
                             <button class="btn btn-primary" type="submit" name="update">Simpan Perubahan</button>
-                            <a href="../datamapel.php">
+                            <a href="dashboard.php">
                                 <button class="btn btn-danger" type="button" name="kembali">Kembali</button>
                             </a>
                         </form>
